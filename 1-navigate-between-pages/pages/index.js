76245abe46f99238baae3735864e27a5fb05abc9
vnduy01 <duynt@sphinx-software.com>
2019-07-25 18:07:26 +0700

    initial commit

--- 1-navigate-between-pages/pages/index.js
+++ 1-navigate-between-pages/pages/index.js
@@ -1,7 +1,12 @@
-export default function Index() {
-  return (
+import Link from 'next/link';
+
+const Index = () => (
     <div>
-      <p>Hello Next.js</p>
+        <Link href="/about" title="About Page">
+            <a>About page</a>
+        </Link>
+        <p>Hello Next.js</p>
     </div>
-  )
-}
+)
+
+export default Index;
--- 3-create-dynamic-pages/pages/index.js
+++ 3-create-dynamic-pages/pages/index.js
@@ -1,9 +1,23 @@
-import Layout from '../components/MyLayout.js'
+import Layout from '../components/MyLayout.js';
+import Link from 'next/link';
 
-export default function Index() {
-  return (
-    <Layout>
-      <p>Hello Next.js</p>
-    </Layout>
-  )
+const PostLink = props => (
+    <li>
+        <Link href = {`/post?title=${props.title}`}>
+            <a>{ props.title }</a>
+        </Link>
+    </li>
+);
+
+export default function Blog() {
+    return (
+        <Layout>
+            <h1>My Blog</h1>
+            <ul>
+                <PostLink title="Hello Next.js"/>
+                <PostLink title="Learn Next.js"/>
+                <PostLink title="Deploy apps with Zeit"/>
+            </ul>
+        </Layout>
+    )
 }
